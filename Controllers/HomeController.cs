c1fb8d95b7d94ca71df51669739909ac747916f2
Yuri Tokio <yuri@sheild.com.br>
2023-09-08 06:09:14 -0300

    Insert, Alter, Delet basics ok!

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,69 +24,146 @@ public class HomeController : Controller
     }
 
     internal TodoViewModel GetAllTodos()
+    {
+        List<TodoItem> todoList = new();
+
+        using (SqliteConnection con =
+               new SqliteConnection("Data Source=db.sqlite"))
         {
-            List<TodoItem> todoList = new();
-
-            using (SqliteConnection con =
-                   new SqliteConnection("Data Source=db.sqlite"))
+            using (var tableCmd = con.CreateCommand())
             {
-                using (var tableCmd = con.CreateCommand())
+                con.Open();
+                tableCmd.CommandText = "SELECT * FROM todo";
+
+                using (var reader = tableCmd.ExecuteReader())
                 {
-                    con.Open();
-                    tableCmd.CommandText = "SELECT * FROM todo";
-
-                    using (var reader = tableCmd.ExecuteReader())
+                    if (reader.HasRows)
                     {
-                        if (reader.HasRows)
+                        while (reader.Read())
                         {
-                            while (reader.Read())
-                            {
-                                todoList.Add(
-                                    new TodoItem
-                                    {
-                                        Id = reader.GetInt32(0),
-                                        Name = reader.GetString(1)
-                                    });
-                            }
+                            todoList.Add(
+                                new TodoItem
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Name = reader.GetString(1)
+                                });
                         }
-                        else
+                    }
+                    else
+                    {
+                        return new TodoViewModel
                         {
-                            return new TodoViewModel
-                            {
-                                TodoList = todoList
-                            };
-                        }
-                    };
-                }
+                            TodoList = todoList
+                        };
+                    }
+                };
             }
-
-            return new TodoViewModel
-            {
-                TodoList = todoList
-            };
         }
+
+        return new TodoViewModel
+        {
+            TodoList = todoList
+        };
+    }
 
     public RedirectResult Insert(TodoItem todo)
+    {
+        using (SqliteConnection con =
+               new SqliteConnection("Data Source=db.sqlite"))
         {
-            using (SqliteConnection con =
-                   new SqliteConnection("Data Source=db.sqlite"))
+            using (var tableCmd = con.CreateCommand())
             {
-                using (var tableCmd = con.CreateCommand())
+                con.Open();
+                tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
+                try
                 {
-                    con.Open();
-                    tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
-                    try
-                    {
-                        tableCmd.ExecuteNonQuery();
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
+                    tableCmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
             }
-            return Redirect("https://localhost:7010/");
+        }
+        return Redirect("https://localhost:7010/");
+    }
+
+    [HttpPost]
+    public JsonResult Delete(int id)
+    {
+        using (SqliteConnection con =
+               new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using (var tableCmd = con.CreateCommand())
+            {
+                con.Open();
+                tableCmd.CommandText = $"DELETE from todo WHERE Id = '{id}'";
+                tableCmd.ExecuteNonQuery();
+            }
         }
 
+        return Json(new { });
+    }
+
+    [HttpGet]
+    public JsonResult PopulateForm(int id)
+    {
+        var todo = GetById(id);
+        return Json(todo);
+    }
+
+    internal TodoItem GetById(int id)
+    {
+        TodoItem todo = new();
+
+        using (var connection =
+                new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using (var tableCmd = connection.CreateCommand())
+            {
+                connection.Open();
+                tableCmd.CommandText = $"SELECT * FROM todo Where Id = '{id}'";
+
+                using (var reader = tableCmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        todo.Id = reader.GetInt32(0);
+                        todo.Name = reader.GetString(1);
+                    }
+                    else
+                    {
+                        return todo;
+                    }
+                };
+            }
+        }
+
+        return todo;
+    }
+
+    public RedirectResult Update(TodoItem todo)
+    {
+        using (SqliteConnection con =
+               new SqliteConnection("Data Source=db.sqlite"))
+        {
+            using (var tableCmd = con.CreateCommand())
+            {
+                con.Open();
+                tableCmd.CommandText = $"UPDATE todo SET name = '{todo.Name}' WHERE Id = '{todo.Id}'";
+                try
+                {
+                    tableCmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        return Redirect("https://localhost:7010/");
+    }
 
 }
